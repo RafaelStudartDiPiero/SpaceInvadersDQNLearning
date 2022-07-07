22780d9faa59256f5227153104f8b6e87d5832c3
RafaelStudartDiPiero <rafael.spiero@gmail.com>
2022-07-07 14:17:31 -0300

    Gym Dependency

---
+++ dqn_agent.py
@@ -0,0 +1,7 @@
+import random
+import numpy as np
+from collections import deque
+from tensorflow.keras import models,layers,optimizers,activations,losses
+
+
+class DQNAgent:
---
+++ train_dqn.py
@@ -0,0 +1,4 @@
+import gym
+import tensorflow as tf
+
+env = gym.make("ALE/SpaceInvaders-v5")
